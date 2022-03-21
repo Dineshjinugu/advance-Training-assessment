8f7012761d649c50150592e96723f105c20c8fb9
Dineshjinugu <97776348+Dineshjinugu@users.noreply.github.com>
2022-03-21 21:05:38 +0530

    Add files via upload

---
+++ Stairs.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Stairs {
+
+	static int fib(int n)
+    {
+        if (n <= 1)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+ 
+    
+    static int countWays(int s)
+    {
+    		return fib(s + 1);
+    }
+ 
+    
+    public static void main(String args[])
+    {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter the no. of steps: ");
+        int s = sc.nextInt();
+        
+        System.out.println("Number of ways = " + countWays(s));
+    }
+}
